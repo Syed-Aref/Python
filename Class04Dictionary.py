4b6520f7d986acad0dda497b6c5decb86c71f3b5
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2020-09-25 00:21:20 +0600

    Update Class04Dictionary.py

--- Class04Dictionary.py
+++ Class04Dictionary.py
@@ -12,35 +12,36 @@ Created on Tue Aug  4 17:21:49 2020
 3. dictionary = {k1:v1,k2:v2,k3:v3, .... ,kn:vn}
 k1 != k2 !=.....!=kn
 v1 = v2 =.....=vn
-4.In a list both keys and values have no restrictions(Any key or value can be used)
+4.In a list both keys and values have no restrictions on type(Any type of key or value can be used)
 '''
 
 #Initializing dictionary
-my_dic1 = {1:'a',2:'b'};
-my_dic2 = {1:'a','A':'b'};
+my_dic1 = {1:'a',2:'b'}
+my_dic2 = {1:'a','A':'b'}
 my_dic3 = {1:'a',2:2}
-my_dic3 = {1:[1,2,3],"abc":2};
+my_dic3 = {1:[1,2,3],"abc":2}
 my_family ={
     "lst" : [1,2,3,4],
     "child" : {"c1" : "a","c2" : "b"}
     } #Exemple of nested dictionary
-my_dic4 = {} #OP : {}
+my_dic4 = {} #{}
 
 #printing dictionary
 print(my_dic1) #OP : {1: 'a', 2: 'b'}
 print(my_dic4)
 
-#printing dictionary key values
+#printing/accessing dictionary key values
 print(my_dic1.get(2)) #OP : b  [value of key 2]
 print(my_dic3.get(1)[2]) #OP : 3 [ key 1 is [1,2,3,4].So, list key 1[2] is 3 ]
+print(my_dic1.get(3)) #OP : None
 
-# Adding Elements in dictionary
+# Adding/changing/Accessing/Popping elements in dictionary
 my_dic2[2] = "i"
 print(my_dic2[2]) #OP : i
 print(my_dic2) #OP : {1: 'a', 'A': 'b', 2: 'i'}. 2:'i' has been added
 print(my_dic2.pop(2)) #OP : i.
 print(my_dic2) #OP : {1: 'a', 'A': 'b'}. 2:'i' has been deleted
-
+#print( my_dic1[3] ) --> will give error
 
 # loop in dictionary
 for i in my_dic3 :
@@ -51,7 +52,7 @@ for i,j in my_dic3.items() :
 #Cheking if key is present in dictionary or not.(Return type is boolean)   
 print(4  in my_dic3) #Will chehck if key 4 is present in dictionry.
 
-#item pair , keys and values
+#printing item(pair) , keys and values
 print(my_dic1.items()) #OP : dict_items([(1, 'a'), (2, 'b')])
 print(my_dic1.keys()) #OP : dict_keys([1, 2])
 print(my_dic1.values()) #OP : dict_values(['a', 'b'])
@@ -61,14 +62,19 @@ dic = {}
 dic = dic.fromkeys([4,2,3],"A")
 print(dic) #OP : {4: 'A', 2: 'A', 3: 'A'}
 
-#list of keys
+#list of keys(getting)
 print( list(dic.keys()) )
 
-#list of values
+#list of values(getting)
 print( list(dic.values()) )
 
-# list of keys in sorted order[Returns list of keys in sorted order]
-print( list( sorted( dic.keys() ) ) ) #OP : [2, 3, 4]
+#list of items/pairs(getting)
+print( list(dic.items()) )
+
+# list of keys in sorted order
+l_ = list( dic.keys() )
+l_.sort()
+print(  l_  ) #OP : [2, 3, 4]
 
 
 
