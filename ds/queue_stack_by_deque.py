f9e49080f9687a83e6ec12928d82311db34167d8
Syed-Aref <64474740+Syed-Aref@users.noreply.github.com>
2023-01-26 15:39:48 +0600

    Update queue_stack_by_deque.py

--- ds/queue_stack_by_deque.py
+++ ds/queue_stack_by_deque.py
@@ -1,9 +1,9 @@
 # We will implement st ad queue data structure using Collections.deque()
 # We will use four methods only(All have time complexity of O(1)):
-    #deq.append(element): appends at the right-end of the deque
-    #deq.appendleft(element): appends at the left-end of the deque
-    #deq.pop(): removes the right-most element and return the element
-    #deq.popleft(): removes the left-most element and return the element
+    # deq.append(element): appends at the ending of the deque(right-end)
+    # deq.appendleft(element): appends at the begining of the deque(left-end)
+    # deq.pop(): removes the last/right-most element and return the element
+    # deq.popleft(): removes the first/left-most element and return the element
 
 
 #Queue:
@@ -17,10 +17,10 @@ queue.append(1)
 queue.append(2)
 queue.append(3)
 
-#print queue
+#Print queue
 print(queue)
 
-#top of the queue
+#First(oldest) element of the queue
 top = queue.popleft()
 queue.appendleft(top)
 print(top)
@@ -43,16 +43,16 @@ stack.append(1)
 stack.append(2)
 stack.append(3)
 
-#print stack
+#Print stack
 print(stack)
 
-#LAST OF THE STACK
+#Last(newest) element of the stack
 last = stack.pop()
 stack.append(last)
 print(last)
 print(stack)
 
-#clearing stack
+#Clearing stack
 while len(stack) > 0:
     stack.pop()
 print(stack)
